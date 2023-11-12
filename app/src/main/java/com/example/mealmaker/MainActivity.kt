3f516e694040a718cbdb0d1cbf24d09cf6429e36
Carlos Sac <carlos.sacmendoza@udc.edu>
2023-11-12 03:20:58 -0500

    updated recipe activity

--- app/src/main/java/com/example/mealmaker/MainActivity.kt
+++ app/src/main/java/com/example/mealmaker/MainActivity.kt
@@ -17,12 +17,12 @@ import okhttp3.Headers
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    val id = "72a9a585"
-    val key = "b2bc0b595313e8d3dcbda591d057ab41"
-    val health = "&health=gluten-free"
-    var ingr = ""
-    val ingList = ""
-
+    private val id = "72a9a585"
+    private val key = "b2bc0b595313e8d3dcbda591d057ab41"
+    private val health = "&health=gluten-free"
+    private var ingr = "chicken"
+    private var ingList = ""
+    private var link = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,8 +34,13 @@ class MainActivity : AppCompatActivity() {
         searchButton.setOnClickListener{
             showResult()
 
+            parseIngr()
+
+            link = "https://api.edamam.com/api/recipes/v2?type=public&q=$ingList&app_id=$id&app_key=$key$health"
+
             val intent = Intent(this, RecipesActivity::class.java)
 
+            intent.putExtra("link", link)
             startActivity(intent)
 
 
@@ -48,8 +53,8 @@ class MainActivity : AppCompatActivity() {
     private fun getMealListURL() {
 
         val client = AsyncHttpClient()
-        parseIngr()
-        val link = "https://api.edamam.com/api/recipes/v2?type=public&q=$ingList&app_id=$id&app_key=$key$health"
+
+
         client[link, object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
                 Log.d("Food", "response successful$json")
@@ -73,15 +78,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun parseIngr(){
         val word = ingr.split(", ", " ")
-        val ingList = word.joinToString("%20")
+        ingList = word.joinToString("%20")
 
     }
 
     private fun showResult(){
-        parseIngr()
+
         val ingText = findViewById<EditText>(R.id.ingr)
         ingr = ingText.text.toString()
-        getMealListURL()
+
+        parseIngr()
 
     }
 }
--- app/src/main/java/com/example/mealmaker/RecipesActivity.kt
+++ app/src/main/java/com/example/mealmaker/RecipesActivity.kt
@@ -2,10 +2,61 @@ package com.example.mealmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class RecipesActivity : AppCompatActivity() {
+    var link = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipes)
+
+        val intent = intent
+        link = intent.getStringExtra("link").toString()
+
+        getMealListURL()
+
+
+    }
+
+    private fun getMealListURL() {
+
+        val client = AsyncHttpClient()
+
+
+        client[link, object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Food", "response successful$json")
+
+                Toast.makeText(applicationContext, "searching", Toast.LENGTH_SHORT).show()
+
+                val linkText = findViewById<TextView>(R.id.linkText)
+
+                linkText.text = link.toString()
+
+
+
+
+
+
+
+
+            }
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Recipe Error", errorResponse)
+            }
+        }]
+
     }
 }
