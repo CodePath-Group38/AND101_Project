ac18e447c37a8298079b251a0851a1548971dbf2
Carlos Sac <carlos.sacmendoza@udc.edu>
2023-11-11 19:00:15 -0500

    added API test request

--- app/src/main/java/com/example/mealmaker/MainActivity.kt
+++ app/src/main/java/com/example/mealmaker/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.mealmaker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.codepath.asynchttpclient.AsyncHttpClient
@@ -14,24 +19,35 @@ class MainActivity : AppCompatActivity() {
     val id = "72a9a585"
     val key = "b2bc0b595313e8d3dcbda591d057ab41"
     val health = "&health=gluten-free"
-    val ingr = "chicken%20potato"
+    var ingr = "chicken, potato"
+    val ingList = ""
 
-    val link = "https://api.edamam.com/api/recipes/v2?type=public&q=$ingr&app_id=$id&app_key=$key$health"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getMealListURL()
+
+        val searchButton = findViewById<TextView>(R.id.search)
+        searchButton.setOnClickListener{
+            showResult()
+        }
+
+
+
     }
 
     private fun getMealListURL() {
 
         val client = AsyncHttpClient()
-
+        parseIngr()
+        val link = "https://api.edamam.com/api/recipes/v2?type=public&q=$ingList&app_id=$id&app_key=$key$health"
         client[link, object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
                 Log.d("Food", "response successful$json")
+
+                Toast.makeText(applicationContext, "searching", Toast.LENGTH_LONG).show()
 
 
 
@@ -45,6 +61,19 @@ class MainActivity : AppCompatActivity() {
                 Log.d("Recipe Error", errorResponse)
             }
         }]
+
+    }
+
+    private fun parseIngr(){
+        val word = ingr.split(", ", " ")
+        val ingList = word.joinToString("%20")
+
+    }
+
+    private fun showResult(){
+        val ingText = findViewById<EditText>(R.id.ingr)
+        ingr = ingText.text.toString()
+        getMealListURL()
 
     }
 }
