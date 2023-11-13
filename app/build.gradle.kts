7a13db7ac1937968e7f71ced05c086feec3173d5
elect <zhuazhu.zz@gmail.com>
2023-11-13 06:41:33 -0500

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,10 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.2.1")
+
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
